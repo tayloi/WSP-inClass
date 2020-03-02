2cb484201b776f75e7d18aac625e14b424c4565c
tayloi <taylor1201am@gmail.com>
2020-03-02 16:29:28 -0500

    Routing - filled login template

--- What Do You Meme/client/src/views/Home.vue
+++ What Do You Meme/client/src/views/Home.vue
@@ -5,15 +5,5 @@
           Home Page
         </h1>
     </div>
-    <div class="card">
-      <div class="card-content">
-        <ul>
-          <li v-for="player in Players" :key="player.Name">
-            {{player.Name}} {{player.Score}}
-          </li>
-        </ul>
-      </div>
-      <img :src="CurrentPicture" class="card-image"/>
-    </div>
   </section>
 </template>
