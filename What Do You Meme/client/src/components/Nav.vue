5fd59595181cf29e5887d248d1e390e6acf3b6b2
tayloi <taylor1201am@gmail.com>
2020-03-07 21:36:41 -0500

    Removed data-target attribute of navbar-burger

--- What Do You Meme/client/src/components/Nav.vue
+++ What Do You Meme/client/src/components/Nav.vue
@@ -5,7 +5,7 @@
       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
     </a>
 
-    <a :class="{ 'is-active':isOpen }" @click="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a :class="{ 'is-active':isOpen }" @click="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
