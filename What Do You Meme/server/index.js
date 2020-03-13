6fe3154e7f6ca66007aa9c912b568cc38d9cb028
tayloi <taylor1201am@gmail.com>
2020-03-12 21:22:08 -0400

    Serving up the client code from the server

--- What Do You Meme/server/index.js
+++ What Do You Meme/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const gameController = require('./controllers/game');
+const path = require('path')
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,16 @@ const port = 3000;
 app
     .use(express.json())
     .use(express.urlencoded({extended: true}))
+    .use(express.static(__dirname + '/../client/dist'))
 //when msg sent w/verb 'get' and '/', call this function
     .get('/', (req, res) => res.send('This class is awesome!')) // <-- lamda expression used here and below
-    .use('/game', gameController);
+    .use('/game', gameController)
+
+    //catch all (if nothing else works)
+    .use((req, res) => {
+        const homePath = path.join( __dirname , '/../client/dist/index.html');
+        console.log(homePath);
+        res.sendFile(homePath)
+    });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
